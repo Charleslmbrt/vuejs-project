a243f2ecd63e5e06c278440c8b41938a2b71fd73
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-07-08 12:38:26 +0200

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+ <my-first-component></my-first-component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MyFirstComponent from './components/MyFirstComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      'my-first-component' : MyFirstComponent
   }
 }
 </script>
---
+++ src/components/MyFirstComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+<p>{{ txt }}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            txt : "Hello World"
+        }
+    }
+}
+</script>
+<style></style>
